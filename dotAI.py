089a77a9d66b33053ac3e1973353cf5cbef75fb1
Saud0227 <Saud02tech@gmail.com>
2021-08-31 15:31:46 +0200

    made window

---
+++ dotAI.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    cv=tk.Canvas(root)
+    cv.pack(fill=tk.BOTH, expand=1)
+    root.geometry("300x500+300+300")
+    
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()

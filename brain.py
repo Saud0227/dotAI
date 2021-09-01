d8d8fce1c0f0058e565e0d1398afaa44e0229b00
Saud0227 <Saud02tech@gmail.com>
2021-09-01 11:38:13 +0200

    created brain class

---
+++ brain.py
@@ -0,0 +1,15 @@
+
+
+
+class brain():
+    def __init__(self,nMoves):
+        print("Brain with {} instructions".format(nMoves))
+
+def main():
+   tst = brain(100)
+
+
+
+
+if __name__ == "__main__":
+    main()

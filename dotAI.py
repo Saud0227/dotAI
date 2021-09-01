8aaeb1f2e20c03fa113bee41ccea169e00d649ff
Saud0227 <Saud02tech@gmail.com>
2021-09-01 11:27:25 +0200

    begun work on dot class

--- dotAI.py
+++ dotAI.py
@@ -1,6 +1,17 @@
 
+
+
+
+class dot():
+    def __init__(self, sPos, gPos, isBest):
+        self.x = sPos[0]
+        self.y = sPos[1]
+        self.g = gPos
+        self.isBest = isBest
+        
 def main():
    print("!") 
+
 
 
 if __name__ == "__main__":

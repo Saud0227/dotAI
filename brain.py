78e75421d69aafedb187d67453678ec5d30270f9
Saud0227 <saud02tech@gmail.com>
2021-09-03 18:38:36 +0200

    basic brain functionallity

--- brain.py
+++ brain.py
@@ -1,12 +1,28 @@
-
-
+from vectors import vector
+from random import randint
 
 class brain():
-    def __init__(self,nMoves):
+
+    @classmethod
+    def fromList(cls, l):
+        tmpB = cls(l.length)
+        tmpB.instructions = l
+        return(tmpB)
+
+    def __init__(self, nMoves):
         print("Brain with {} instructions".format(nMoves))
+        self.instructions = []
+        self.nMoves = nMoves
+
+
+    def rndInstructions(self):
+        for i in range(self.nMoves):
+            self.instructions.append(vector.make2D(randint(0, 360)))
+        for i in self.instructions:
+            print(str(i.x) +";"+ str(i.y))
 
 def main():
-   tst = brain(100)
+   tst = brain(100).rndInstructions()
 
 
 

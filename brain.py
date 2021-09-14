ab3e59b40d6613a391d838802f7802ad8e72ef38
Saud0227 <Saud02tech@gmail.com>
2021-09-14 14:06:17 +0200

    Added brain mutate functionallity

--- brain.py
+++ brain.py
@@ -1,5 +1,5 @@
 from vectors import vector
-from random import randint
+from random import randint, uniform
 
 class brain():
 
@@ -7,6 +7,7 @@ class brain():
     def __init__(self, nMoves, inst = " "):
         self.instructions = []
         self.nMoves = nMoves
+        self.mutatationRate = 0.01
         if isinstance(inst,list):
             self.instructions = inst
             assert (len(self.instructions) == self.nMoves), "Instruction length longer than nMoves"
@@ -18,11 +19,17 @@ class brain():
         for i in range(self.nMoves):
             self.instructions.append(vector.make2D(randint(0, 360)))
 
+    def mutate(self):
+        for i in range(len(self.instructions)):
+            if uniform(0,1)<self.mutatationRate:
+                self.instructions[i] = vector.make2D(randint(0, 360))
+
 
 def main():
    tst1 = brain(100)
    tst1.rndInstructions()
-   tst2 = brain(100,tst1.instructions)
+   tst2 = brain(len(tst1.instructions),tst1.instructions)
+   tst2.mutate()
 
 
 

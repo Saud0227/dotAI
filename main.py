50c2a54d06531f425788646341570ac24db404f2
Saud0227 <Saud02Tech@gmail.com>
2021-10-11 13:24:45 +0200

    started work on sim class

---
+++ main.py
@@ -0,0 +1,18 @@
+from vectors import vector
+
+class sim():
+    def __init__(self, popS, grSize, gPos):
+        pass
+
+
+
+
+
+def main():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
